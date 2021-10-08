40aea2f06c3212f659b6de2e91d7a66422f544d5
rsmnarts <rismanalkarim@gmail.com>
2021-10-08 18:56:03 +0700

    support difference password

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ console.log("");
 console.log("");
 
 const file = readline.question("Enter the file name of your IP list : ");
-const pass = readline.question("Enter your SSH password : ");
 const command = readline.question("input command for all ssh : ");
 
 console.log("");
 console.log("");
 
 getIP(file).then(res => {
-  sshCom(res, pass, command).then(resSSH => {
+  console.log(res)
+  sshCom(res[0], res[1], command).then(resSSH => {
     console.log(resSSH);
   });
 });
